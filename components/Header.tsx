7eb2ca2057b88dbab58b1804a46a0ee59b245a24
fransteven <fransteven18@hotmail.com>
2025-12-31 13:13:33 -0500

    implement authentication logic

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import NavItems from './NavItems'
 import UserDropDown from './UserDropDown'
 
 
-function Header() {
+function Header({user}:{user:User}) {
   return (
     <header className='sticky top-0 header'>
       <div className='container header-wrapper'>
@@ -14,7 +14,7 @@ function Header() {
         <nav className='hidden sm:block'>
           <NavItems />
         </nav>
-        <UserDropDown />
+        <UserDropDown user={user}/>
       </div>
     </header>
   )
